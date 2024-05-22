4baebaa0e31913d6fab26b63e49df636c499a8c5
Inshra23 <164750803+Inshra23@users.noreply.github.com>
2024-05-22 16:00:02 +0500

    my practice

---
+++ script.js
@@ -0,0 +1,92 @@
+//assignment
+//QUESTION 1
+
+let city = +prompt("Enter City")
+if(city === "Karachi"){
+    console.log("Welcome to city of lights");
+}
+else{
+    console.log("Welcome to another city");
+}
+
+//QUESTION 2
+
+let gender = prompt("Enter your Gender")
+if(gender === "Male"){
+    console.log("Good Morning Sir ");
+}
+else if(gender === "female"){
+    console.log("Good Morning Ma'am");
+}
+else{
+    console.log("Other");
+}
+
+//QUESTION 3
+
+let signals = prompt("Enter signal color")
+if(signals === "Red"){
+    alert("Must Stop")
+}
+else if(signals === "Yellow"){
+    alert("Ready to move")
+}
+else if(signals === "Green"){
+    alert("Move now")
+}
+else{
+    alert("On the way")
+}
+
+//CHAPTER 15
+
+// var city0 = "Atlanta";
+// var city1 = "Baltimore";
+// var city2 = "Chicago";
+// var city3 = "Denver";
+// var city4 = "Los Angeles";
+// var city5 = "Seattle";
+// console.log("Welcome to " + city3);
+
+// let cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"]
+// console.log(cities[0].length);
+// console.log(cities[1].length);
+
+// let number = [1,2,3,4,5,6,7]
+// console.log(number);
+// console.log(number[2].length);//undefined
+
+// let info = ["inshra", 19, "adeena", 21, "haiqa",22]
+// console.log(info);
+// console.log(info[0].length);
+// console.log(info[1].length);//undefined
+
+// let mixedArray = [1, "BOB", "Now is", true]
+// console.log(mixedArray);
+// console.log(mixedArray[0].length);//undefined
+// console.log(mixedArray[1].length);
+// console.log(mixedArray[2].length);
+// console.log(mixedArray[4].length);//undefined
+//if we consider number as string it should we defined like this "1"
+
+
+//CHAPTER 16
+
+//Arrays Method()
+let cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"]
+// cities.push("Pakistan", "India"); //add something
+// console.log(cities);
+// cities.pop("India"); //remove something
+// console.log(cities);
+// cities.pop("Pakistan")
+// console.log(cities);
+ 
+console.log(cities);
+console.log(cities.toString()); //to convert into string
+
+
+
+
+
+
+
